d7f83847a77f4425ed75d99a2c2852aa5024cff9
raiesbo <raiesbo@gmail.com>
2023-12-03 12:49:01 +0100

    resolve 2023 2b exercice wih rust

--- 2023/ex2/src/main.rs
+++ 2023/ex2/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    _ex_2a();
+    _ex_2b();
 }
 
 fn read_file(file_path: &str) -> String {
@@ -33,6 +33,34 @@ fn _ex_2a() {
         if is_valid_round {
             result += i + 1;
         }
+    }
+    println!("Result: {:?}", result);
+}
+
+fn _ex_2b() {
+    let content: String = read_file("./src/2a-data");
+    let mut result = 0;
+    for round in content.split("\n") {
+        let mut min_red = 1;
+        let mut min_blue = 1;
+        let mut min_green = 1;
+        let values_string = round.split(": ").nth(1).unwrap();
+        for set in values_string.split("; ") {
+            for cubes in set.split(", ") {
+                let cube_num = cubes.split(" ").nth(0).unwrap().parse().unwrap();
+                let cube_type = cubes.split(" ").nth(1).unwrap();
+                if cube_type == "blue" && min_blue < cube_num {
+                    min_blue = cube_num
+                }
+                if cube_type == "green" && min_green < cube_num {
+                    min_green = cube_num
+                }
+                if cube_type == "red" && min_red < cube_num {
+                    min_red = cube_num
+                }
+            }
+        }
+        result += min_red * min_blue * min_green;
     }
     println!("Result: {:?}", result);
 }
