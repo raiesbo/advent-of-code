9215be59b62ec71ad7d19e681489b27fea8b8dcf
raiesbo <raiesbo@gmail.com>
2024-12-10 22:41:26 +0100

    clean 2024 day 10 b

--- 2024/day_10/day10_b.go
+++ 2024/day_10/day10_b.go
@@ -8,25 +8,25 @@ func (m *Map) CalcTrailsPlus() int {
 	for i, row := range m.data {
 		for j, val := range row {
 			if val == 0 {
-				m.dfsMultiTrails(i, j+1, 1, i, j)
-				m.dfsMultiTrails(i, j-1, 1, i, j)
-				m.dfsMultiTrails(i+1, j, 1, i, j)
-				m.dfsMultiTrails(i-1, j, 1, i, j)
+				m.dfsMultiTrails(i, j+1, 1)
+				m.dfsMultiTrails(i, j-1, 1)
+				m.dfsMultiTrails(i+1, j, 1)
+				m.dfsMultiTrails(i-1, j, 1)
 			}
 		}
 	}
 	return m.numPaths
 }
 
-func (m *Map) dfsMultiTrails(x int, y int, val int, oX int, oY int) {
+func (m *Map) dfsMultiTrails(x int, y int, val int) {
 	if x >= 0 && y >= 0 && x < m.height && y < m.width && m.data[x][y] == val {
 		if val == PICK {
 			m.numPaths++
 		} else {
-			m.dfsMultiTrails(x, y+1, val+1, oX, oY)
-			m.dfsMultiTrails(x, y-1, val+1, oX, oY)
-			m.dfsMultiTrails(x+1, y, val+1, oX, oY)
-			m.dfsMultiTrails(x-1, y, val+1, oX, oY)
+			m.dfsMultiTrails(x, y+1, val+1)
+			m.dfsMultiTrails(x, y-1, val+1)
+			m.dfsMultiTrails(x+1, y, val+1)
+			m.dfsMultiTrails(x-1, y, val+1)
 		}
 	}
 }
